915e25ecdad26d141c525a65187dcb6bdeba537c
begyy <begymrx@gmail.com>
2020-04-09 20:54:39 +0500

    add initialization

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PaycomUz",
-    version="2.1",
+    version="2.2",
     author="Sadullayev Bekhzod",
     author_email="begymrx@gmail.com",
     description="Paycomuz",
