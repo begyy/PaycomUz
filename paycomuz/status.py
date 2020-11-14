8b5ea214ef3318cefcfc8341b3322762f1bf4fa8
Bekhzod <44405438+begyy@users.noreply.github.com>
2020-11-14 17:26:30 +0500

    Revert "Hello world"

--- paycomuz/status.py
+++ paycomuz/status.py
@@ -1,5 +1,5 @@
-ORDER_NOT_FOUND = -31050
-TRANSACTION_NOT_FOUND = -31003
+ORDER_NOT_FOND = -31050
+TRANSACTION_NOT_FOND = -31003
 UNABLE_TO_PERFORM_OPERATION = -31008
 INVALID_AMOUNT = -31001
 ORDER_FOUND = 200
@@ -8,12 +8,12 @@ CREATE_TRANSACTION = 1
 CLOSE_TRANSACTION = 2
 CANCEL_TRANSACTION = -1
 
-ORDER_NOT_FOUND_MESSAGE = {
+ORDER_NOT_FOND_MESSAGE = {
     "uz":"Buyurtma topilmadi",
     "ru":"Заказ не найден",
     "en":"Order not fond"
 }
-TRANSACTION_NOT_FOUND_MESSAGE = {
+TRANSACTION_NOT_FOND_MESSAGE = {
     "uz":"Tranzaksiya topilmadi",
     "ru":"Транзакция не найдена",
     "en":"Transaction not fond"
